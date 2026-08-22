74b8cfe2f40161c22deb09c067032e579bbf2058
IhabChouaibi <ihebch315@gmail.com>
2026-08-22 16:47:11 +0100

    test deployment

--- src/main/java/recruitment/dev/employeeservice/web/EmployeeRoleController.java
+++ src/main/java/recruitment/dev/employeeservice/web/EmployeeRoleController.java
@@ -14,33 +14,37 @@ import recruitment.dev.employeeservice.service.EmployeeRoleService;
 @RestController
 @RequestMapping("/employee-role")
 @RequiredArgsConstructor
-@PreAuthorize("hasRole('HR')")
 public class EmployeeRoleController {
     private final EmployeeRoleService employeeRoleService;
 
+    @PreAuthorize("hasRole('HR')")
     @PostMapping("/create")
     public ResponseEntity<EmployeeRoleDto> createEmployeeRole(   @Valid @RequestBody EmployeeRoleDto employeeRoleDto) {
         EmployeeRoleDto role = employeeRoleService.createEmployeeRole(employeeRoleDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(role);
     }
 
+    @PreAuthorize("hasRole('HR')")
     @PutMapping("/update/{id}")
     public ResponseEntity<EmployeeRoleDto> updateEmployeeRole(@PathVariable Long id,
                                                               @Valid @RequestBody EmployeeRoleDto employeeRoleDto) {
         return ResponseEntity.ok(employeeRoleService.updateEmployeeRole(employeeRoleDto, id));
     }
 
+    @PreAuthorize("hasAnyRole('HR', 'MANAGER')")
     @GetMapping("/get/{id}")
     public ResponseEntity<EmployeeRoleDto> getEmployeeRoleById(@PathVariable Long id) {
         return ResponseEntity.ok(employeeRoleService.getEmployeeRoleById(id));
     }
 
+    @PreAuthorize("hasAnyRole('HR', 'MANAGER')")
     @GetMapping("/getall")
     public ResponseEntity<Page<EmployeeRoleDto>> getAllEmployeeRoles( @RequestParam(defaultValue = "0") int page,
                                                                       @RequestParam(defaultValue = "10") int size) {
         return ResponseEntity.ok(employeeRoleService.getAllEmployeeRole(Pageable.ofSize(size).withPage(page)));
     }
 
+    @PreAuthorize("hasRole('HR')")
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteEmployeeRole(@PathVariable Long id) {
         employeeRoleService.deleteEmployeeRole(id);
